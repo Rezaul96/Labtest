8aa4f0943726ca0cd300af6d4b80ae9813dcc9ab
Rezaul Islam <rezaul0920@gmail.com>
2019-08-22 15:26:37 +0600

    Project Setup

--- LabTest.Api/LabTest.Api/Startup.cs
+++ LabTest.Api/LabTest.Api/Startup.cs
@@ -26,6 +26,10 @@ namespace LabTest.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+
+          
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ LabTest.Api/LabTest.Repository/Core/LabTextDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabTest.Repository.Core
+{
+    public class LabTextDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public LabTextDBContext(DbContextOptions<LabTextDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
